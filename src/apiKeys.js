7f42d9d97dee8a4fbb750dc31b738e728c45b140
Abhishek Kumar <abhishekverman3459@gmail.com>
2023-05-22 02:03:55 +0530

    "Generalized the API KEY"

--- src/apiKeys.js
+++ src/apiKeys.js
@@ -1,5 +1,5 @@
 // Visit https://api.openweathermap.org & then signup to get our API keys for free
 module.exports = {
-  key: "d04cef56daba9b9ea6b6c220b3f2c807",
+  key: "Enter Your API Key Here",
   base: "https://api.openweathermap.org/data/2.5/",
 };
